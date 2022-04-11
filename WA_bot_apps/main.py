39b4217d169ffd6a5de27766441aa64c9659e903
hidayah24 <taufik.hidayah240920@gmail.com>
2022-04-11 09:18:49 +0700

    prepare for the projecr

---
+++ WA_bot_apps/main.py
@@ -0,0 +1,18 @@
+import pyautogui as pt
+import pyperclip as pc
+from time import sleep
+
+class WhatsApp:
+    #Defines the starting values
+    def __init__(self, speed=.5, clik_speed=.4):
+        self.speed = speed
+        self.click_speed = clik_speed
+        self.massage = ''
+        self.last_massage = ''
+    def nav_green_dot(self):
+        #
+
+
+# if __name__ == '__main__':
+#     # print_hi('This is my new project')
+
